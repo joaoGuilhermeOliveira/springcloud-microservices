9df7d7d17c939c812584957905cd9ea1fdca5477
joaoGuilhermeOliveira <joaogoliveira14@gmail.com>
2024-10-24 11:11:18 -0300

    comunicação síncrona com ms cartões, via OpenFeign

--- msavaliadorcredito/src/main/java/io/github/joaoguilherme/msavaliadorcredito/application/AvaliadorCreditoService.java
+++ msavaliadorcredito/src/main/java/io/github/joaoguilherme/msavaliadorcredito/application/AvaliadorCreditoService.java
@@ -1,21 +1,29 @@
 package io.github.joaoguilherme.msavaliadorcredito.application;
 
+import io.github.joaoguilherme.msavaliadorcredito.domain.model.CartaoCliente;
 import io.github.joaoguilherme.msavaliadorcredito.domain.model.DadosCliente;
 import io.github.joaoguilherme.msavaliadorcredito.domain.model.SituacaoCliente;
+import io.github.joaoguilherme.msavaliadorcredito.infra.clients.CartoesResourceClient;
 import io.github.joaoguilherme.msavaliadorcredito.infra.clients.ClienteResourceClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class AvaliadorCreditoService {
 
     private final ClienteResourceClient clienteResourceClient;
+    private final CartoesResourceClient cartoesResourceClient;
 
     public SituacaoCliente obterSituacaoCliente(String cpf) {
         ResponseEntity<DadosCliente> dadosClienteResponse = clienteResourceClient.dadosCliente(cpf);
-        return SituacaoCliente.builder().cliente(dadosClienteResponse.getBody()).build();
+        ResponseEntity<List<CartaoCliente>> cartoesResponse = cartoesResourceClient.getCartoesByCliente(cpf);
+        cartoesResourceClient.getCartoesByCliente(cpf);
+        return SituacaoCliente.builder().cliente(dadosClienteResponse.getBody()).cartoes(cartoesResponse.getBody()).build();
     }
+
 
 }
